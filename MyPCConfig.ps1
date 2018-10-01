00e44c48c80dd3d7ee3c5c302baba8e194038805
Abhishek Bhattacharya <ab7607@rediffmail.com>
2018-10-01 15:23:37 +0530

    Added command alternatives to CimClass using basic environment variables

--- MyPCConfig.ps1
+++ MyPCConfig.ps1
@@ -1,5 +1,7 @@
 clear
-write-host "------------This PowerShell script display the Computername and IP address-----------" -foregroundcolor Yellow
+write-host "This PowerShell script display the Computername and IP address" -foregroundcolor Yellow
 write-host "==============================================================================" -foregroundcolor Yellow
 $g=gwmi -Class Win32_NetworkAdapterConfiguration
-write-host "Computer Name:" $g[17].PSComputerName is assigned the IP Address $g[17].IPAddress
+write-host "Computer Name:" $g[17].PSComputerName is assigned the `nIP Address $g[17].IPAddress
+write-host "You can also access the Computername using gci Env: command" -foregroundcolor yellow
+read-host
